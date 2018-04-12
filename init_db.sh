bbb615e6833e3f3763af817f7a9242288bb1e5a4
ak04nv <ak04nv@gmail.com>
2018-04-12 14:00:44 +0800

    typo fixed in init_db script

--- init_db.sh
+++ init_db.sh
@@ -7,5 +7,5 @@ rm $DB
 cat schema.sql | sqlite3 $DB
 (echo .mode csv; echo .import $FILE mkb10) | sqlite3 $DB
 for field in 'id_parent_id' 'code' 'date'; do
-  echo "update mkb10 set $field = NULL where length($field) = 0" | sqlite3 $DB
+  echo "update mkb10 set $field = NULL where length($field) = 0;" | sqlite3 $DB
 done
